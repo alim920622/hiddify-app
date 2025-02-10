dffe5db650ac6edca4e5a440a997fffb227442ad
alim920622 <121553733+alim920622@users.noreply.github.com>
2025-02-10 12:06:01 +0500

    Update SingBox.swift

--- ios/HiddifyPacketTunnel/SingBox/SingBox.swift
+++ ios/HiddifyPacketTunnel/SingBox/SingBox.swift
@@ -33,7 +33,7 @@ class SingBox {
         json["inbounds"] = [
             [
                 "type": "tun",
-                "inet4_address": "172.19.0.1/30",
+                "inet4_address": "192.168.43.176/30",
                 "auto_route": true,
                 "mtu": mtu,
                 "sniff": true
